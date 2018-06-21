b18f482eeb6f62f2f858ab2fd0f6d8d809b40043
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2018-06-21 08:48:07 +0800

    Add files via upload

--- Hexbin-Plot.R
+++ Hexbin-Plot.R
@@ -1,32 +1,39 @@
 # "Пчелиные соты" - гексагональный график распределения значений данных по ячейкам сот.
 
 # ЧАСТЬ-1. готовим датафрейм. 
-	# шаг-1. вчитываем таблицу с данными. делаем из нее исходный датафрейм. чистим датафрейм от NA
-
-
+	# шаг-1. вчитываем таблицу с данными. делаем из нее исходный датафрейм.
 MDepths <- read.csv("Depths.csv", header=TRUE, sep = ",")
 Ml <- na.omit(MDepths) 
 row.has.na <- apply(Ml, 1, function(x){any(is.na(x))}) 
 sum(row.has.na) 
 head(Ml)
-
+    # шаг-2. сшиваем столбцы по категориям
 MDTt = melt(setDT(Ml), measure = patterns("^profile"), value.name = c("depth"))
 head(MDTt)
-
 
 library(hexbin) 
 
 # ЧАСТЬ-2. рисуем диаграмму-"пчелиные соты".
-
+# 1 вариант ,через library(hexbin)
 x = as.factor(MDTt$variable)
 y = MDTt$depth
-
 hexbinplot(y ~ x, MDTt, aspect = 1,
            trans = sqrt, inv = function(x) x^2)
- 
-
 x = MDF$profile
 y = MDF$Min
 
 hexbinplot(y ~ x, MDF, aspect = 1,
            trans = sqrt, inv = function(x) x^2)
+
+# 2 вариант
+bin <- hexbin(x, y)
+plot(bin, style = "nested.lattice")
+
+plot(bin, style = "nested.lattice")
+
+# статистика по пчелиному графику
+hbi <- hexbin(y ~ x, xbins = 80, IDs= TRUE)
+str(hbi)
+tI <- table(hbi@cID)
+stopifnot(names(tI) == hbi@cell,
+tI  == hbi@count)
